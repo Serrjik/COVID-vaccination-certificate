8d8091b61bfa69d0bd06c1fe084206e4fe77b8a0
Serrjik <uac.sarge@gmail.com>
2021-08-24 09:15:26 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// Путь к файлу index.html.
+const url = require('url').format({
+	protocol: 'file',
+	slashes: true,
+	// Глобальная переменная __dirname указывает на папку с проектом.
+	pathname: require('path').join(__dirname, 'index.html'),
+})
+
+const { app, BrowserWindow } = require('electron')
+
+// Состояние приложения.
+let win
+
+// Функция создаёт окно приложения.
+function createWindow() {
+	win = new BrowserWindow({
+		width: 1820,
+		height: 1040,
+		// icon: './icon.png',
+	})
+
+	// Что нужно отобразить в приложении.
+	win.loadURL(url)
+
+	// Что будет при нажатии на кнопку "Закрыть"?
+	win.on('closed', function () {
+		win = null
+	})
+}
+
+// Повесить на приложение обработчик события готовности запуска приложения.
+app.on('ready', createWindow)
+
+// Для Mac и Linux, чтобы приложение закрывалось, а не уходило в спящий режим.
+app.on('window-all-closed', function () {
+	app.quit()
+})
